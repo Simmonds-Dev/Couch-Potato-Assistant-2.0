b60f00191e0fec32463a759862a988ca2795d5f6
Christoph551 <christophersimmonds551@gmail.com>
2025-04-01 18:44:13 -0700

    Saving some style changes to results

--- js/index.js
+++ js/index.js
@@ -1,19 +1,33 @@
-let API_KEY = '8b30258'; // OMDb API key
 const userInput = document.getElementById('userInput');
 const searchBtn = document.getElementById('searchBtn');
-const resultsContainer = document.getElementById('results');
+// const modal = document.getElementById('modal');
+// const modalMessage = document.getElementById('modalMessage');
+// const closeModal = document.getElementById('closeModal');
+const resultsContainer = document.getElementById('resultsContainer');
+
+// function showModal(message) {
+//     modalMessage.textContent = message;
+//     modal.style.display = "block";
+// }
 
 // Ensure button does not trigger form submission
 const form = document.querySelector('.searchField');
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent page reload
     const query = userInput.value.trim();
-    if (query) {
+    if (query === "") {
+        showModal("Oops! Please ensure to fill out the input field before searching.")
+    } {
         fetchMovie(query);
     }
 });
 
+// closeModal.addEventListener("click", () => {
+//     modal.style.display = "none";
+// });
+
 async function fetchMovie(query) {
+    const API_KEY = '8b30258'; 
     const url = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${encodeURIComponent(query)}`;
 
     try {
@@ -34,11 +48,20 @@ async function fetchMovie(query) {
 function displayMovie(data) {
     resultsContainer.innerHTML = `
     <section class="results">
-        <h2>${data.Title} (${data.Year})</h2>
-        <img class="poster" src="${data.Poster !== 'N/A' ? data.Poster : './assets/placeholder.jpg'}" alt="${data.Title} Poster">
-            <p><strong>Genre:</strong> ${data.Genre}</p>
-            <p><strong>Plot:</strong> ${data.Plot}</p>
-            <p><strong>IMDB Rating:</strong> ${data.imdbRating}</p>
-        </section>
+        <aside class="posterAndTitle">
+            <h2>${data.Title} (${data.Year})</h2>
+            <img class="poster" src="${data.Poster !== 'N/A' ? data.Poster : './assets/placeholder.jpg'}" alt="${data.Title} Poster">
+        </aside>
+        <aside class="movieDetails">
+            <section class"details">
+                <p><strong>Genre:</strong> ${data.Genre}</p>
+                <p><strong>Actors:</strong> ${data.Actors}</p>
+                <p><strong>Plot:</strong> ${data.Plot}</p>
+                <p><strong>IMDB Rating:</strong> ${data.imdbRating}</p>
+                <p><strong>Box Office:</strong> ${data.BoxOffice}</p>
+                <p><strong>Runtime:</strong> ${data.Runtime}</p>
+            </section>
+        </aside>
+    </section>
     `;
 }
